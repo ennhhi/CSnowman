0da74c0daf2d0afb6055b73bbe1afb3c7fa3524b
rika <76397078+Nyriki@users.noreply.github.com>
2023-05-13 22:19:06 -0700

    Update CSnowmanGame.cpp

--- CSnowmanGame.cpp
+++ CSnowmanGame.cpp
@@ -9,7 +9,8 @@ using namespace std;
 
 // === CSnowmanGame::CSnowmanGame ====================
 //
-// ﻿This is the CSnowmanGame default constructor. It sets up the environment by initializing the private data members by calling the Reset function.
+// This is the CSnowmanGame default constructor. It sets up the environment 
+// by initializing the private data members by calling the Reset function.
 //
 // Input:
 //		none
@@ -20,7 +21,13 @@ using namespace std;
 
 // === CSnowmanGame::Start ===========================
 //
-// This function starts the game. It clears the terminal screen to show only the game by calling the function system("clear"). It further calls member functions to display the title of the game, draw the snowman figure, draw available letters, draw the number of tries, and the word to guess in dashed lines. The Start function will loop continuously until the user guesses correctly or has no more tries. This function will display a message to the user if they have guessed the word(s) correctly or ran out of tries.
+// This function starts the game. It clears the terminal screen to show only 
+// the game by calling the function system("clear"). It further calls member 
+// functions to display the title of the game, draw the snowman figure, draw 
+// available letters, draw the number of tries, and the word to guess in dashed 
+// lines. The Start function will loop continuously until the user guesses 
+// correctly or has no more tries. This function will display a message to the 
+// user if they have guessed the word(s) correctly or ran out of tries.
 //
 // Input:
 //		none
@@ -35,7 +42,8 @@ using namespace std;
 
 // === CSnowmanGame::Reset ===========================
 //
-// This function initializes all the private data members. It will use CWords's object to randomly obtain a word and update m_wordToGuess.
+// This function initializes all the private data members. It will use 
+// CWords's object to randomly obtain a word and update m_wordToGuess.
 //
 // Input:
 //		none
@@ -49,7 +57,11 @@ using namespace std;
 
 // === CSnowmanGame::DrawAvailLetters ================
 //
-// ﻿This function displays the letters over two rows that the user has not guessed yet. At the start of the game, the function will display all the letters in the alphabet. As the user takes their guesses, the letters will be replaced with a space. This will keep the structure/spacing of all the letters the same.
+// This function displays the letters over two rows that the user has not 
+// guessed yet. At the start of the game, the function will display all the 
+// letters in the alphabet. As the user takes their guesses, the letters 
+// will be replaced with a space. This will keep the structure/spacing of 
+// all the letters the same.
 //
 // Input:
 //		none
@@ -64,7 +76,11 @@ using namespace std;
 
 // === CSnowmanGame::DispWordDash ====================
 //
-// ﻿This function displays the word to guess with underscores. As the user guesses each correct letter, the function appropriately updates the dash to the letter guessed. As a note, only alphabetical characters are expressed with underscores. If the word contains anything else, like a space, period, or a number, make sure that is not hidden and displayed.
+// This function displays the word to guess with underscores. As the user 
+// guesses each correct letter, the function appropriately updates the dash 
+// to the letter guessed. As a note, only alphabetical characters are expressed 
+// with underscores. If the word contains anything else, like a space, 
+// period, or a number, make sure that is not hidden and displayed.
 //
 // Input:
 //		none
@@ -79,7 +95,9 @@ using namespace std;
 
 // === CSnowmanGame::GetUserGuess ====================
 //
-// This function promprs the user to enter a guess. It then validates the guess by calling the IsValidGuess. If the guess is not valid, it will repeatedly ask the user to enter a valid guess.
+// This function promprs the user to enter a guess. It then validates the 
+// guess by calling the IsValidGuess. If the guess is not valid, it will 
+// repeatedly ask the user to enter a valid guess.
 //
 // Input:
 //		none
@@ -94,7 +112,14 @@ using namespace std;
 
 // === CSnowmanGame::IsValidGuess ====================
 //
-// This function checks if the guess the user entered is valid. The function will return false if the user enters a non-alphabetical character or if the user entered the same letter before. None of these two cases should count against the user's tries. The function returns true if the guess has not been entered before, whether the guess is correct or not. For each case, the function will display an appropriate message and sleep for SLEEP_NUM_SNOWMAN seconds, to let the user read the message before moving on with the program.
+// This function checks if the guess the user entered is valid. The function 
+// will return false if the user enters a non-alphabetical character or if 
+// the user entered the same letter before. None of these two cases should 
+// count against the user's tries. The function returns true if the guess 
+// has not been entered before, whether the guess is correct or not. For 
+// each case, the function will display an appropriate message and sleep 
+// for SLEEP_NUM_SNOWMAN seconds, to let the user read the message before 
+// moving on with the program.
 //
 // Input:
 //		guess	[IN]	-- a char to represent the guess of the user
@@ -109,7 +134,8 @@ using namespace std;
 
 // === CSnowmanGame::GetNumUniqueLetters =============
 //
-// This function calculates the number of unique letters for the word to guess. It also changes the location of each letter position in m_lettersInWord as true.
+// This function calculates the number of unique letters for the word to guess. 
+// It also changes the location of each letter position in m_lettersInWord as true.
 //
 // Input:
 //		none
@@ -124,7 +150,8 @@ using namespace std;
 
 // === CSnowmanGame::DrawSnowman =====================
 //
-// This function draws the appropriate stage/part of the snowman based of the number of tries the user has.
+// This function draws the appropriate stage/part of the snowman based of the 
+// number of tries the user has.
 //
 // Input:
 //		numTries	[In]	-- the number of tries the user has so far
